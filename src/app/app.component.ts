cf5eaea96ae861d8f34d515a6672831848eeb026
Charun <kingring23@hotmail.com>
2023-05-14 19:24:51 +0530

    changes added for xlsx filereader.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { AuditLogComponent } from './audit-log.component';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +11,15 @@ import { AuditLogComponent } from './audit-log.component';
 })
 export class AppComponent implements OnInit {
   title = 'angular-ag-grid-project';
+  myForm!: FormGroup;
+  selectedFile: File | null = null;
 
-  constructor(private dialog: MatDialog) {
+  constructor(private dialog: MatDialog, private fb: FormBuilder) {
     // constructor
+    this.myForm = fb.group({
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required]
+    });
   }
   ngOnInit(): void {
     // oninint
@@ -21,5 +29,31 @@ export class AppComponent implements OnInit {
       width: '100%',
       disableClose: true
     });
+  }
+  resetForm(): void {
+    this.myForm.reset();
+  }
+
+  onFileSelected(event: any): void {
+      this.selectedFile = event.target.files[0] ?? null;
+      console.log(this.selectedFile);
+      // const file =async (e: any) => {
+      //   const workbook = XLSX.read(await e.files.arrayBuffer(), {type: 'binary'}); 
+      // }
+      // for (i = 0, f = files[i]; i != files.length; ++i) {
+        if (this.selectedFile) {
+          const reader = new FileReader();
+          const name = this.selectedFile.name;
+          reader.onload = function(e: ProgressEvent<FileReader>) {
+              if (e.target) {
+                const data = e.target.result;
+                const workbook = XLSX.read(data, {type: 'binary'});
+                console.log(workbook);
+              }
+              /* DO SOMETHING WITH workbook HERE */
+          };
+          reader.readAsBinaryString(this.selectedFile);
+        }
+    // }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { AgGridModule } from 'ag-grid-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuditLogComponent } from './audit-log.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,8 @@ import { AuditLogComponent } from './audit-log.component';
     MatSidenavModule,
     MatDialogModule,
     MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
     AgGridModule.withComponents([]),
     BrowserAnimationsModule,
   ],
